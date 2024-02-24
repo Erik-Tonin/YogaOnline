7f3c842f56e607930d028e8bb88a9653cfc95a98
tonin.dev <eriktonin.dev@gmail.com>
2024-02-24 14:28:12 -0300

    CreateTeacher

---
+++ YogaOnline/Controllers/TeacherController.cs
@@ -0,0 +1,52 @@
+﻿using Application.Contracts;
+using Application.DTOs;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using YogaOnline.Domain.Entities;
+
+namespace YogaOnline.Controllers
+{
+    public class TeacherController : ApiController
+    {
+        private readonly ITeacherApplicationService _teacherApplicationService;
+        private readonly IMapper _mapper;
+
+        public TeacherController(ITeacherApplicationService teacherApplicationService, IMapper mapper)
+        {
+            _teacherApplicationService = teacherApplicationService;
+            _mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("RegisterTeacher")]
+        public async Task<Teacher> RegisterTeacher(Teacher teacher)
+        {
+            Teacher teacherRegister = await _teacherApplicationService.Register(teacher);
+
+            return teacherRegister;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("GetAllSegmentation")]
+        public async Task<IEnumerable<TeacherDTO>> GetAllSegmentation()
+        {
+            return await _teacherApplicationService.GetAllSegmentation();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("GetByIdSegmentation")]
+        public async Task<IEnumerable<TeacherDTO>> GetByIdSegmentation(int id)
+        {
+            return await _teacherApplicationService.GetByIdSegmentation(id);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("GetPagedTeachers")]
+        public async Task<IEnumerable<TeacherDTO>> GetPagedTeachers(TeacherDTO teacherDTO)
+        {
+            var teachers = await _teacherApplicationService.GetPagedTeachers(teacherDTO.PageIndex, teacherDTO.PageSize);
+            return _mapper.Map<IEnumerable<TeacherDTO>>(teachers);
+        }
+    }
+}
--- YogaOnline/Program.cs
+++ YogaOnline/Program.cs
@@ -1,5 +1,6 @@
 using Application.Contracts;
 using Application.Implementations;
+using Application.Mappings;
 using Microsoft.EntityFrameworkCore;
 using YogaOnline.Domain.Contracts.IRepositories;
 using YogaOnline.Infra.Context;
@@ -15,13 +16,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
 //Services
 builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+builder.Services.AddScoped<ITeacherApplicationService, TeacherApplicationService>();
 
 //Repository
+builder.Services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
 builder.Services.AddScoped<IUserRepository, UserRepositorie>();
+builder.Services.AddScoped<ITeacherRepository, TeacherRepository>();
 builder.Services.AddDbContext<MicroServiceContext>(options =>
     options.UseMySQL(builder.Configuration.GetConnectionString("MySqlConnection")));
 
